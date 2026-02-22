16219686bda9876a1ae0e070dddef7c0a4797b1c
Mr.Arm Dev <thinnawutgaming@gmail.com>
2026-02-22 14:58:35 +0700

    Update roblox_config.py

--- roblox_config.py
+++ roblox_config.py
@@ -14,26 +14,21 @@ def _save(data):
     with open(FILE, "w") as f:
         json.dump(data, f, indent=2)
 
-def set_status_config(guild_id, universe_id, channel_id):
+def add_game(guild_id, universe_id, group_id, channel_id):
     data = _load()
     guild = data.setdefault(str(guild_id), {})
-    guild["universe_id"] = universe_id
-    guild["channel_id"] = channel_id
-    guild.setdefault("message_id", None)
-    guild.setdefault("group_id", None)
+    guild[str(universe_id)] = {
+        "universe_id": universe_id,
+        "group_id": group_id,
+        "channel_id": channel_id,
+        "message_id": None
+    }
     _save(data)
 
-def set_message_id(guild_id, message_id):
-    data = _load()
-    if str(guild_id) in data:
-        data[str(guild_id)]["message_id"] = message_id
-        _save(data)
+def get_games(guild_id):
+    return _load().get(str(guild_id), {})
 
-def set_group_id(guild_id, group_id):
+def set_message_id(guild_id, universe_id, message_id):
     data = _load()
-    guild = data.setdefault(str(guild_id), {})
-    guild["group_id"] = group_id
+    data[str(guild_id)][str(universe_id)]["message_id"] = message_id
     _save(data)
-
-def get_status_config(guild_id):
-    return _load().get(str(guild_id))
