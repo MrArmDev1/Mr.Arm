c374717e346774ed31699735b914d2557fa67073
Mr.Arm Dev <thinnawutgaming@gmail.com>
2026-02-22 13:13:24 +0700

    Update guild_config.py

--- guild_config.py
+++ guild_config.py
@@ -1,4 +1,20 @@
-# ----- ROBLOX STATUS -----
+import json
+import os
+
+FILE = "guild_config.json"
+
+def _load():
+    if not os.path.exists(FILE):
+        with open(FILE, "w") as f:
+            json.dump({}, f)
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+def _save(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=2)
+
+# ---------- ROBLOX STATUS ----------
 
 def set_status_config(guild_id, universe_id, channel_id):
     data = _load()
@@ -6,8 +22,16 @@ def set_status_config(guild_id, universe_id, channel_id):
     guild["roblox_status"] = {
         "universe_id": universe_id,
         "channel_id": channel_id,
-        "message_id": None
+        "message_id": None,
+        "group_id": None
     }
+    _save(data)
+
+def set_group(guild_id, group_id):
+    data = _load()
+    guild = data.setdefault(str(guild_id), {})
+    if "roblox_status" in guild:
+        guild["roblox_status"]["group_id"] = group_id
     _save(data)
 
 def get_status_config(guild_id):
